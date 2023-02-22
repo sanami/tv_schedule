315ba72ff43d2d78bb4399aa5d4f9df5480a6e39
sa <sanami77@gmail.com>
2023-02-22 23:59:13 +0600

    Items filter

--- lib/tv_schedule.ex
+++ lib/tv_schedule.ex
@@ -1,11 +1,30 @@
 defmodule TvSchedule do
   @host URI.parse("https://tv.mail.ru")
+  @ignore_names ["Әзіл студио", "31 Әзіл"]
 
   def get_channel(channel) do
     url = URI.merge(@host, "/astana/channel/#{channel}/")
     %{status_code: 200, body: body} = HTTPoison.get! url
 
+    File.mkdir_p("tmp")
+    File.write("tmp/#{channel}.html", body)
+
     body
+  end
+
+  def process_item(time, name, today) do
+    time = Time.from_iso8601!("#{time}:00")
+    time_attrs = time |> Map.from_struct |> Map.delete(:calendar)
+
+    date_time = Map.merge(today, time_attrs)
+    date_time = if time.hour < 5 do
+
+      DateTime.add(date_time, 60*60*24, :second) #TODO :day
+    else
+      date_time
+    end
+
+    %{ time: date_time, name: name }
   end
 
   def parse_channel(html, today \\ DateTime.utc_now) do
@@ -17,18 +36,7 @@ defmodule TvSchedule do
         name = Floki.find(item_el, ".p-programms__item__name-link") |> Floki.text
 
         if String.length(time) > 0 do
-          time = Time.from_iso8601!("#{time}:00")
-          time_attrs = time |> Map.from_struct |> Map.delete(:calendar)
-
-          date_time = Map.merge(today, time_attrs)
-          date_time = if time.hour < 5 do
-
-            DateTime.add(date_time, 60*60*24, :second) #TODO :day
-          else
-            date_time
-          end
-
-          %{ time: date_time, name: name }
+          process_item(time, name, today)
         end
       end)
     |> Enum.filter(&is_map/1)
@@ -42,13 +50,19 @@ defmodule TvSchedule do
       end)
   end
 
+  def filter_items(items) do
+    Enum.filter(items, fn item ->
+      ((item.time.hour in 19..23) || (item.time.hour in 0..2)) && item.duration > 77 && !(item.name in @ignore_names)
+    end)
+  end
+
   def run do
     for channel <- [1644, 1606, 1502] do
       IO.puts channel
 
       get_channel(channel)
       |> parse_channel
-      |> Enum.filter(fn item -> ((item.time.hour in 18..23) || (item.time.hour in 0..2)) && item.duration > 77 end)
+      |> filter_items
       |> IO.inspect
     end
 
--- test/tv_schedule_test.exs
+++ test/tv_schedule_test.exs
@@ -1,19 +1,38 @@
 defmodule TvScheduleTest do
   use ExUnit.Case
+  import TvSchedule
 
   test "get_channel" do
-    html = String.length(TvSchedule.get_channel(1644))
+    html = String.length(get_channel(1644))
     res = String.length(html)
     IO.inspect res
+
     assert res > 10_000
   end
 
   test "parse_channel" do
     html1 = File.read! "test/fixtures/1644.html"
 
-    res = TvSchedule.parse_channel(html1)
+    res = parse_channel(html1)
     IO.inspect res
+
     assert length(res) == 15
+  end
+
+  test "process_item" do
+    today = DateTime.utc_now
+    res = process_item("18:30", "name1", today)
+    IO.inspect res
+
+    %{name: name, time: time} = res
+    assert name == "name1"
+  end
+
+  test "filter_items" do
+    html1 = File.read! "test/fixtures/1644.html"
+
+    res = parse_channel(html1) |> filter_items
+    IO.inspect res
   end
 
 #  test "run", do: TvSchedule.run
