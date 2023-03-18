4887fb4933a201ad49f4ceba801a2c449bc42170
sa <sanami77@gmail.com>
2023-03-18 17:02:52 +0600

    Change app name

--- config/config.exs
+++ config/config.exs
@@ -6,11 +6,11 @@ config :logger,
     [level_lower_than: :info]
   ]
 
-config :ex_tools,
+config :tv_schedule,
   key1: "value1",
   key2: "value2"
 
-# Application.fetch_env!(:ex_tools, :key1)
+# Application.fetch_env!(:tv_schedule, :key1)
 
 env_file = "#{config_env()}.exs"
 # IO.puts env_file
--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule TvSchedule.MixProject do
 
   def project do
     [
-      app: :ex_tools,
+      app: :tv_schedule,
       escript: escript_config(),
       version: "0.1.0",
       elixir: "~> 1.12",
